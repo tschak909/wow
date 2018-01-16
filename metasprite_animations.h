e92fadc11491b602dbcb2b5763d0dd4ee13e82e8
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2018-01-16 00:20:44 -0600

    Fix the animation frames for shooting states for blue and yellow players.

--- metasprite_animations.h
+++ metasprite_animations.h
@@ -16,14 +16,14 @@ const unsigned char metasprite_animation_data[]={
   2,2,2,2,     // Worrior Blue Up Idle
   3,7,11,3,   // Worrior Blue Down
   3,3,3,3,     // Worrior Blue Down Idle
-  4,12,16,12,  // Worrior Blue Right Shooting
-  4,12,16,12,  // Worrior Blue Right Shooting Idle
-  5,13,17,17,  // Worrior Blue Left Shooting
-  5,13,17,17,  // Worrior Blue Left Shooting Idle
-  6,14,18,14,  // Worrior Blue Up Shooting
-  6,14,18,14,  // Worrior Blue Up Shooting Idle
-  7,15,19,15,  // Worrior Blue Down Shooting
-  7,15,19,15,  // Worrior Blue Down Shooting Idle
+  4,8,12,16,  // Worrior Blue Right Shooting
+  4,8,12,16,  // Worrior Blue Right Shooting Idle
+  5,9,13,17,  // Worrior Blue Left Shooting
+  5,9,13,17,  // Worrior Blue Left Shooting Idle
+  6,10,14,18,  // Worrior Blue Up Shooting
+  6,10,14,18,  // Worrior Blue Up Shooting Idle
+  7,11,15,19,  // Worrior Blue Down Shooting
+  7,11,15,19,  // Worrior Blue Down Shooting Idle
   // -------------------------------------------
   20,24,28,28, // Burwor Right
   21,25,29,29, // Burwor Left
@@ -53,14 +53,14 @@ const unsigned char metasprite_animation_data[]={
   64,64,64,64, // Worrior Yellow Up Idle
   65,69,73,65, // Worrior Yellow Down
   65,65,65,65, // Worrior Yellow Down Idle
-  66,70,74,70, // Worrior Yellow Right Shooting
-  66,70,74,70, // Worrior Yellow Right Shooting Idle
+  66,70,74,78, // Worrior Yellow Right Shooting
+  66,70,74,78, // Worrior Yellow Right Shooting Idle
   67,71,75,75, // Worrior Yellow Left Shooting
   67,71,75,75, // Worrior Yellow Left Shooting Idle
-  68,72,76,72, // Worrior Yellow Up Shooting
-  68,72,76,72, // Worrior Yellow Up Shooting Idle  
-  69,73,77,73, // Worrior Yellow Down Shooting
-  69,73,77,73, // Worrior Yellow Down Shooting Idle
+  68,72,76,80, // Worrior Yellow Up Shooting
+  68,72,76,80, // Worrior Yellow Up Shooting Idle  
+  69,73,77,81, // Worrior Yellow Down Shooting
+  69,73,77,81, // Worrior Yellow Down Shooting Idle
 };
 
 #endif /* METASPRITE_ANIMATIONS_H */
